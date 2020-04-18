1fbf85db3c1caa459812e346d0cb4b8601f6e653
ericltwee <ericltwee@gmail.com>
2020-04-18 22:45:27 +0800

    links added

--- src/parts/contact.js
+++ src/parts/contact.js
@@ -3,14 +3,49 @@ import React from "react";
 const Contact = () => {
   return (
     <div>
-      <div className="iconContainer">
-        <i class="fab fa-github fa-3x"></i>
-        <i class="far fa-envelope fa-3x"></i>
-        <i class="fab fa-linkedin-in fa-3x"></i>
-        <i class="fab fa-instagram fa-3x"></i>
-        <i class="fab fa-whatsapp fa-3x"></i>
+      <a
+        href="https://www.facebook.com/ericltwee"
+        class="facebook"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <i class="fab fa-facebook-f fa-3x"></i>
-      </div>
+      </a>
+      <a href="#" class="whatsapp">
+        <i class="fab fa-whatsapp fa-3x"></i>
+      </a>
+      <a
+        href="https://www.instagram.com/ericltwee"
+        class="instagram"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="fab fa-instagram fa-3x"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/ericltwee/"
+        class="linkedin"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="fab fa-linkedin-in fa-3x"></i>
+      </a>
+      <a
+        href="mailto:ericltwee@gmail.com"
+        class="email"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="far fa-envelope fa-3x"></i>
+      </a>
+      <a
+        href="https://github.com/ericltwee"
+        class="github"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="fab fa-github fa-3x"></i>
+      </a>
 
       <h1 className="wallText">CONTACT</h1>
     </div>
--- src/parts/skills.js
+++ src/parts/skills.js
@@ -4,12 +4,12 @@ const Skills = () => {
   return (
     <div>
       <div className="iconContainer">
-        <i class="fab fa-html5 fa-3x"></i>
-        <i class="fab fa-css3-alt fa-3x"></i>
-        <i class="fab fa-react fa-3x"></i>
-        <i class="fab fa-js-square fa-3x"></i>
-        <i class="fab fa-python fa-3x"></i>
-        <i class="fab fa-sass fa-3x"></i>
+        <i class="fab fa-html5 fa-3x">HTML</i>
+        <i class="fab fa-css3-alt fa-3x">CSS</i>
+        <i class="fab fa-react fa-3x">REACT</i>
+        <i class="fab fa-js-square fa-3x">JAVASCRIPT</i>
+        <i class="fab fa-python fa-3x">PYTHON</i>
+        <i class="fab fa-sass fa-3x">SASS</i>
       </div>
 
       <h1 className="wallText">SKILLS</h1>
