ed7666b73919bce4c8967f9efe7a056dbf1f6208
ericltwee <ericltwee@gmail.com>
2020-04-18 18:37:25 +0800

    bloody responsive worktab

--- src/parts/School.js
+++ src/parts/School.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
+import kdu from "../images/kdu.png";
+import next from "../images/0next.png";
 
 const School = () => {
   return (
     <div>
       <Container className="workbox">
-        <Row>
-          <Col sm className="workcard">
-            loremx20
-          </Col>
-          <Col sm className="workcard">
-            sm=4
-          </Col>
-          <Col sm className="workcard">
-            sm=true
-          </Col>
+        <Row className="workcard">
+          <img className="brandlogo" src={next} alt="logo"></img>
+        </Row>
+        <Row className="workcard">
+          <img className="brandlogo" src={kdu} alt="logo"></img>
+        </Row>
+        <Row className="workcard">
+          <img className="brandlogo" src={kdu} alt="logo"></img>
         </Row>
       </Container>
       <h1 className="wallText">EDUCATION</h1>
--- src/parts/Work.js
+++ src/parts/Work.js
@@ -1,41 +1,68 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import "./Work.css";
+import srcc from "../images/SR.png";
+import mbs from "../images/mbs.jpg";
+import club from "../images/clubmed.jpg";
 
 const Work = () => {
   return (
     <div>
       <Container className="workbox">
-        <Row>
-          <Col sm className="workcard">
+        <div className="workcard">
+          <div className="workside">
+            <img className="brandlogo" src={srcc} alt="logo"></img>
             <p>Manager</p>
-            <p>Secret Recipe Cakes & Cafes, Malaysia</p>
-            <p>
+          </div>
+          <div className="workdetails">
+            <div className="worktitle">
+              <h3 className="workplace">Secret Recipe, Malaysia</h3>
+              <p>Nov 2011 -Nov 2019</p>
+            </div>
+            <p className="descript">
               Cafe Manager of a franchise outlet in the KlangValley.
               Responsibilities include staff management, task delegations,
-              recruitment & training, inventory & stock management, overseeing
-              product quality, service standards and business performance.
+              recruitment and training, inventory and stock management,
+              overseeing product quality, service standards and business
+              performance.
             </p>
-          </Col>
-          <Col sm className="workcard">
+          </div>
+        </div>
+        <div className="workcard">
+          <div className="workside">
+            <img className="brandlogo" src={mbs} alt="logo"></img>
             <p>Butler</p>
-            <p>Marina Bay Sands, Singapore</p>
-            <p>
+          </div>
+          <div className="workdetails">
+            <div className="worktitle">
+              <h3 className="workplace">Marina Bay Sands, Singapore</h3>
+              <p>Jan - Jun 2011</p>
+            </div>
+            <p className="descript">
               Assigned as a main point of contact for guests throughout their
               stay. Providing personalized service while working/coordinating
               with other hotel departments. Trained to be a "jack of all
               trades".
             </p>
-          </Col>
-          <Col sm className="workcard">
+          </div>
+        </div>
+        <div className="workcard">
+          <div className="workside">
+            <img className="brandlogo" src={club} alt="logo"></img>
             <p>Intern</p>
-            <p>Club Med Cherating, Malaysia</p>
-            <p>
-              6 months internship in various departments within the
-              resort.Housekeeping, Front-desk, Restaurants, and the Bar.
+          </div>
+          <div className="workdetails">
+            <div className="worktitle">
+              <h3 className="workplace">Club Med Cherating, Malaysia</h3>
+              <p>Jan - Jun 2010</p>
+            </div>
+            <p className="descript">
+              6 months internship in various departments within the resort. Area
+              of training includes housekeeping, front-desk, restaurants, and
+              the bar.
             </p>
-          </Col>
-        </Row>
+          </div>
+        </div>
       </Container>
 
       <h1 className="wallword">EXPERIENCE</h1>
