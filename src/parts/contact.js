bc08cbdce56da30bdebcf5dfab74ebe14fd79328
ericltwee <ericltwee@gmail.com>
2020-04-20 22:40:46 +0800

    contact alignment

--- src/parts/contact.js
+++ src/parts/contact.js
@@ -1,51 +1,74 @@
 import React from "react";
+import "../parts/contact.css";
 
 const Contact = () => {
   return (
     <div>
-      <a
-        href="https://www.facebook.com/ericltwee"
-        class="facebook"
-        target="_blank"
-        rel="noopener noreferrer"
+      <form
+        method="post"
+        action="https://getform.io/{your-unique-getform-endpoint}"
       >
-        <i class="fab fa-facebook-f fa-3x"></i>
-      </a>
-      <a href="#" class="whatsapp">
-        <i class="fab fa-whatsapp fa-3x"></i>
-      </a>
-      <a
-        href="https://www.instagram.com/ericltwee"
-        class="instagram"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i class="fab fa-instagram fa-3x"></i>
-      </a>
-      <a
-        href="https://www.linkedin.com/in/ericltwee/"
-        class="linkedin"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i class="fab fa-linkedin-in fa-3x"></i>
-      </a>
-      <a
-        href="mailto:ericltwee@gmail.com"
-        class="email"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i class="far fa-envelope fa-3x"></i>
-      </a>
-      <a
-        href="https://github.com/ericltwee"
-        class="github"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <i class="fab fa-github fa-3x"></i>
-      </a>
+        <label>
+          Email
+          <input type="email" name="email" />
+        </label>
+        <label>
+          Name
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Message
+          <input type="text" name="message" />
+        </label>
+      </form>
+      <div className="contactbox mx-auto">
+        <div className="contactinfo">
+          <i class="fas fa-map-marker-alt fa-3x"></i>
+          <p>Petaling Jaya, Selangor</p>
+        </div>
+        <div className="contactinfo">
+          <i class="fab fa-whatsapp fa-3x"></i>
+          <p>+6016 215 7746</p>
+        </div>
+        <div className="contactinfo">
+          <i class="far fa-envelope fa-3x"></i>
+          <p>ericltwee@gmail.com</p>
+        </div>
+      </div>
+      <div className="contactbox">
+        <a
+          href="https://www.facebook.com/ericltwee"
+          class="facebook"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i class="fab fa-facebook-f fa-3x"></i>
+        </a>
+        <a
+          href="https://www.instagram.com/ericltwee"
+          class="instagram"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i class="fab fa-instagram fa-3x"></i>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/ericltwee/"
+          class="linkedin"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i class="fab fa-linkedin-in fa-3x"></i>
+        </a>
+        <a
+          href="https://github.com/ericltwee"
+          class="github"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i class="fab fa-github fa-3x"></i>
+        </a>
+      </div>
 
       <h1 className="wallText">CONTACT</h1>
     </div>
